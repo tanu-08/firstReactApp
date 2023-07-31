b274b2e7278b9c07a47081ed1ed3c3b69c49d23d
HP <tanu.bhardwaj@agicent.in>
2023-07-31 16:36:17 +0530

    added class  component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
-class App extends React.Component{
+export default class App extends React.Component{
   render(){
     return <h1>Class Component</h1>
   }
 }
-export default App;
+
